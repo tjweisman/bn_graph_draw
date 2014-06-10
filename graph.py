9ba9571bc2d1963e1a5639f61196862f407f2923
Teddy Weisman <tjweisman@gmail.com>
2014-06-10 16:35:20 -0400

    started implementing some gonality computations

--- graph.py
+++ graph.py
@@ -9,6 +9,7 @@ except ImportError as e:
 
 try:
     from sympy.matrices import *
+    from sympy import floor
 except ImportError as e:
     sympy_ok = False
 
@@ -16,7 +17,8 @@ class Point:
     pass
 
 class Vertex:
-    #number of vertices defined so far
+    #number of vertices defined so far.
+    #this will break if we define more than one graph!
     count = 0
     
     #vertex display information
@@ -55,6 +57,9 @@ class Graph:
         self.edges = []
         #laplacian is None whenever it's out-of-date
         self.Q = None
+        # SymPy version of laplacian (so we don't call the matrix constructor
+        # unnecessarily)
+        self.SymQ = None
     def add_vertex(self, x=0, y=0):
         self.vertices.append(Vertex(x,y))
         self.Q = None
@@ -74,6 +79,12 @@ class Graph:
     def get_last(self):
         return self.vertices[-1]
 
+    def sym_laplacian(self):
+        if self.Q:
+            return self.SymQ
+        self.laplacian()
+        return self.SymQ
+        
     def laplacian(self):
         # if we already have a valid laplacian don't bother recomputing it
         if self.Q:
@@ -86,6 +97,7 @@ class Graph:
             self.Q[j][i] -= 1
             self.Q[i][i] += 1
             self.Q[j][j] += 1
+        self.SymQ = Matrix(self.Q)
         return self.Q
         
     def jacobian(self):
@@ -98,7 +110,7 @@ class Graph:
         if not sympy_ok:
             return "SymPy needed to compute pairing guess"
         n = len(self.vertices)
-        Q = Matrix(self.laplacian()) #this is SymPy!
+        Q = self.SymQ #this is SymPy!
         J = ones(n,n)
 
         #Moore-Penrose pseudoinverse
@@ -128,3 +140,34 @@ class Graph:
         self.edges = []
         self.Q = None
         Vertex.count = 0
+
+# a divisor on a graph
+class Divisor:
+    def __init__(self, graph):
+        self.graph = graph
+        self.values = ([0]*len(graph.vertices))[:]
+        self.degree = 0
+    def set(self, vertex, value):
+        self.values[vertex.i] = value
+    def get(self, vertex):
+        return self.values[vertex.i]
+    def compute_degree(self):
+        self.degree = sum(self.values)
+
+    #implementation of Algorithm 5.1 in Bruyn '12
+    def reduced(self):
+        n = len(self.graph.vertices)
+        x = Divisor(self.graph)
+        if len(self.graph.vertices) <= 1:
+            return
+        for vertex in self.graph.vertices[1:]:
+            x.set(vertex, vertex.deg - self.get(vertex))
+        x.set(self.graph.vertices[0], v0)
+        #S for SymPy
+        xS = Matrix(n - 1, 1, x.values[1:])
+        QS = self.graph.sym_laplacian()
+        yS = zeros(1).col_join(QS.minorMatrix(0,0).LUsolve(xS))
+        vals = [x + y for x,y in zip(self.values, QS * yS.applyfunc(floor))]
+        d = Divisor(self.graph)
+        d.values = vals
+        return d
