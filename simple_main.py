0c4f9c572b24b8c8d4bd015df9ea4944f0cd4329
Teddy Weisman <tjweisman@gmail.com>
2015-06-17 11:15:17 -0400

    renamed file

--- simple_main.py
+++ simple_main.py
@@ -4,17 +4,15 @@ from graph_ui.graph_frame import GraphFrame
 import wx
 
 app = wx.App()
+frame=GraphFrame('Graphmake')
+frame.add_infobox("adj","Adjacency matrix")
+f_graph = frame.view.graph
 
 def adj_mat(evt):
     global f_graph
-    frame.set_infobox("adj",str(f_graph.adjacency()))    
+    frame.set_infobox("adj",str(f_graph.adjacency()))
 
-frame=GraphFrame('Graphmake')
-frame.add_infobox("adj","Adjacency matrix")
 frame.add_button("Compute adjacency matrix", adj_mat)
-f_graph = frame.view.graph
-
 
 frame.Show()
-
 app.MainLoop()
